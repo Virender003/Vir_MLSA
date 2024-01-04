0e5a14204e379381b4472e37ee1fbf9f39a58118
Virender Singh Rawat <rawatvirender434@gmail.com>
2024-01-04 17:26:53 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,26 +16,24 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/mountains-2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Mouontain Background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a passionate programmer and i believe in learning new skills and implement them practically.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "User Interface",
+  "Frontend",
   "Graphic design",
 ];
 
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my Portfolio website where you can know about me and the skills i have.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/mountains.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "mountains background";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Blog website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is the Blog website.",
+    url: "https://mindfulmoments001.blogspot.com/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Vir's Instagram",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is the instagram of Virender Singh Rawat",
+    url: "https://www.instagram.com/vir_451/",
   },
   {
-    title: "My Resume Site",
+    title: "My Github site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is the Github of Virender Singh Rawat",
+    url: "https://github.com/Virender003",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Vir's Linkedin",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is the Linkedin of Virender Singh Rawat.",
+    url: "https://www.linkedin.com/in/virender-rawat-894aa0228",
   },
 ];
 
